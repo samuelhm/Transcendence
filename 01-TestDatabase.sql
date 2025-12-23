d9328e0567cdb79fb27370d5e26079291e6d53ce
shurtado <shurtado@student.42barcelona.com>
2025-12-23 03:34:24 +0100

    No datos aqui

--- 01-TestDatabase.sql
+++ 01-TestDatabase.sql
@@ -1,10 +1,6 @@
--- database/01-init.sql
-
--- 1. Configuración inicial y extensiones
--- Necesario para generar UUIDs v4 (gen_random_uuid())
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- 2. Creación de la Tabla (Basado en tu dump)
+
 CREATE TABLE IF NOT EXISTS public.users (
     id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
     nickname character varying(50) NOT NULL,
@@ -25,29 +21,4 @@ CREATE TABLE IF NOT EXISTS public.users (
     CONSTRAINT users_nickname_key UNIQUE (nickname)
 );
 
--- 3. Restauración de Datos (Data Seed)
--- Insertamos tu usuario actual para que tu Token JWT siga siendo válido.
--- Usamos 'ON CONFLICT DO NOTHING' para evitar errores si reinicias el contenedor sin borrar el volumen.
-INSERT INTO public.users (
-    id, 
-    nickname, 
-    email, 
-    password_hash, 
-    first_name, 
-    last_name, 
-    avatar_path, 
-    is_active
-) VALUES (
-    '2f39b1eb-b741-4a44-8ea3-6099383238c2', -- TU UUID ACTUAL
-    'vendedor1',
-    'dev@example.com',
-    '1234', -- Tu contraseña actual (texto plano para dev)
-    'Juan',
-    'Perez',
-    NULL,
-    true
-) ON CONFLICT (id) DO NOTHING;
-
--- 4. Permisos (Solución al error 42501)
--- Garantizamos que cualquier usuario conectado (como 'admin' del docker-compose) pueda tocar la tabla.
 GRANT ALL PRIVILEGES ON TABLE public.users TO public;
